9a173d4393100d2749639059ddd4b5fa36ba7685
Nicholas Salesky <nicksalesky@gmail.com>
2023-07-29 18:33:54 -0700

    Drawing a triangle

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,18 @@ use glium::glutin::event::{Event, WindowEvent};
 use glium::glutin::event_loop::EventLoop;
 use glium::glutin::window::WindowBuilder;
 use glium::glutin::ContextBuilder;
-use glium::{Display, Surface};
+use glium::index::{NoIndices, PrimitiveType};
+use glium::uniforms::EmptyUniforms;
+use glium::{Display, Program, Surface, VertexBuffer};
+
+#[macro_use]
+extern crate glium;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+implement_vertex!(Vertex, position);
 
 pub fn run() {
     let events_loop = EventLoop::new();
@@ -15,8 +26,56 @@ pub fn run() {
 
     let display = Display::new(wb, cb, &events_loop).unwrap();
 
+    // FIXME:
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+    };
+    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex_buffer = VertexBuffer::new(&display, &shape).unwrap();
+    let indices = NoIndices(PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program =
+        Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
     let mut frame = display.draw();
     frame.clear_color(0.0, 0.0, 1.0, 1.0);
+
+    frame
+        .draw(
+            &vertex_buffer,
+            &indices,
+            &program,
+            &EmptyUniforms,
+            &Default::default(),
+        )
+        .unwrap();
+
     frame.finish().unwrap();
 
     events_loop.run(move |event, _, control_flow| match event {
